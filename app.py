cb20017cb303d802e2130a2d0bdc3f199e849444
Kunal Baviskar <kunalvasantbaviskar1997@gmail.com>
2025-06-09 18:51:08 +0530

    update commit

--- app.py
+++ app.py
@@ -5,13 +5,19 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
+import time
 
 # Load environment variables from .env file
 load_dotenv()
+max_retries = 10
+retry_interval = 3
+
+# update your database name and connection strings
+db_name = "kbvskrdb"        
+
+server_connection_string = "Driver={ODBC Driver 17 for SQL Server};Server=tcp:kbvskr-mssqlserver.database.windows.net,1433;Uid=dbadmin;Pwd=Kbvskr@12345;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"
 
 db_connection_string = "Driver={ODBC Driver 17 for SQL Server};Server=tcp:kbvskr-mssqlserver.database.windows.net,1433;Database=kbvskrdb;Uid=dbadmin;Pwd=Kbvskr@12345;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"
-
-db_name = "kbvskrdb"
 
 # server_connection_string = (
 #     "DRIVER={ODBC Driver 17 for SQL Server};"
@@ -48,31 +54,54 @@ class Task(BaseModel):
 # @app.get("/api")
 @app.on_event("startup")
 def create_tasks_table():
-    i=0
+    print(f"⏳ Creating {db_name} database..")
     try:
         #Connect without db to create it
-        # with pyodbc.connect(server_connection_string, autocommit=True) as conn:
-        #     cursor = conn.cursor()
-        #     cursor.execute(f"CREATE DATABASE {db_name}")
-        #     i+=1
+        with pyodbc.connect(server_connection_string, autocommit=True) as conn:
+            cursor = conn.cursor()
+            cursor.execute(f"IF DB_ID('{db_name}') IS NULL CREATE DATABASE {db_name}")
+
+        for attempt in range(max_retries):
+            try:
+                with pyodbc.connect(db_connection_string, autocommit=True) as conn:
+                    print(f"✅ Database {db_name} is now created.")
+                    break
+            except pyodbc.OperationalError as e:
+                print(f"⏳ Waiting for DB to be ready... Attempt {attempt + 1}")
+                time.sleep(retry_interval)
+        else:
+            raise Exception("❌ Database not accessible after multiple retries.")
 
         # Connect to DB and create table
         with pyodbc.connect(db_connection_string, autocommit=True) as conn:
             cursor = conn.cursor()
             cursor.execute("""
+                IF OBJECT_ID('Tasks', 'U') IS NULL
                 CREATE TABLE Tasks (
                     ID int NOT NULL PRIMARY KEY IDENTITY,
                     Title varchar(255),
                     Description text
                 );
             """)
-            i+=1
+            print("✅ DB table is created. Tasks API is ready.")
     except Exception as e:
         print(e)
-    if i == 1:
-        return "Database and Tables are created. Tasks API is ready."
-    else:
-        return "Error creating database or table."
+
+# List all tasks
+@app.get("/api")
+def get_tasks():
+    tasks = []
+    with pyodbc.connect(db_connection_string) as conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM Tasks")
+        for row in cursor.fetchall():
+            task = {
+                "ID": row.ID,
+                "Title": row.Title,
+                "Description": row.Description
+            }
+            tasks.append(task)
+    return tasks
 
 # List all tasks
 @app.get("/api/tasks")
